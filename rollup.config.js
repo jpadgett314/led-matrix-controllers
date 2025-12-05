93c3d483946fa91a80c9ad879572f980a516b497
Jacob Padgett <jacobapadgett@gmail.com>
2025-12-06 07:05:55 +0900

    build: added UMD build

--- rollup.config.js
+++ rollup.config.js
@@ -3,11 +3,19 @@ import terser from '@rollup/plugin-terser';
 
 export default {
   input: 'src/index.js',
-  output: {
-    file: 'dist/led-matrix-controllers.browser.mjs',
-    format: 'esm',
-    sourcemap: true
-  },
+  output: [
+    {
+      file: 'dist/led-matrix-controllers.browser.mjs',
+      format: 'esm',
+      sourcemap: true
+    },
+    {
+      file: 'dist/led-matrix-controllers.umd.min.js',
+      format: 'umd',
+      name: 'LedMatrixControllers',
+      sourcemap: true
+    }
+  ],
   plugins: [
     resolve(),
     terser(),
